b6c37d8e2fcc17cfabb8052c0ae89a336a8f6968
Lucas Gazzola <85223876+lucasjg017@users.noreply.github.com>
2021-10-29 19:43:44 -0300

    add project info

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,23 @@
+import Card from 'react-bootstrap/Card'
+import proyectsInfo from './proyectsInfo'
+
+export default function ProjectCard () {
+  return (
+    proyectsInfo.map(proyect => {
+      const { id, url, name, description, image } = proyect
+      return (
+        <a className='text-decoration-none text-dark' key={id} href={url} target='_blank' rel='noreferrer'>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant='top' src={image} />
+            <Card.Body>
+              <Card.Title>{name}</Card.Title>
+              <Card.Text>
+                {description}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </a>
+      )
+    })
+  )
+}
---
+++ src/components/proyectsInfo.js
@@ -0,0 +1,39 @@
+const proyectsInfo = [
+  {
+    id: 1,
+    url: 'https://www.google.com',
+    name: 'Proyecto 1',
+    description: 'Este es el proyecto numero uno',
+    image: 'https://dhakasetup.com/images/300x300.png'
+  },
+  {
+    id: 2,
+    name: 'Proyecto 2',
+    url: 'https://www.google.com',
+    description: 'Este es el proyecto numero dos',
+    image: 'https://dhakasetup.com/images/300x300.png'
+  },
+  {
+    id: 3,
+    name: 'Proyecto 3',
+    url: 'https://www.google.com',
+    description: 'Este es el proyecto numero tres',
+    image: 'https://dhakasetup.com/images/300x300.png'
+  },
+  {
+    id: 4,
+    name: 'Proyecto 4',
+    url: 'https://www.google.com',
+    description: 'Este es el proyecto numero cuatro',
+    image: 'https://dhakasetup.com/images/300x300.png'
+  },
+  {
+    id: 5,
+    name: 'Proyecto 5',
+    url: 'https://www.google.com',
+    description: 'Este es el proyecto numero cinco',
+    image: 'https://dhakasetup.com/images/300x300.png'
+  }
+]
+
+export default proyectsInfo
