3e348e9b79609f09081a79f206459ff677df537b
Lucas Gazzola <85223876+lucasjg017@users.noreply.github.com>
2021-11-01 10:53:44 -0300

    minor fix

--- src/components/proyectsInfo.js
+++ src/components/proyectsInfo.js
@@ -1,7 +1,7 @@
 const proyectsInfo = [
   {
     id: 1,
-    url: 'https://www.google.com',
+    url: '#',
     name: 'Proyecto 1',
     description: 'Este es el proyecto numero uno',
     image: 'https://dhakasetup.com/images/300x300.png'
@@ -9,28 +9,28 @@ const proyectsInfo = [
   {
     id: 2,
     name: 'Proyecto 2',
-    url: 'https://www.google.com',
+    url: '#',
     description: 'Este es el proyecto numero dos',
     image: 'https://dhakasetup.com/images/300x300.png'
   },
   {
     id: 3,
     name: 'Proyecto 3',
-    url: 'https://www.google.com',
+    url: '#',
     description: 'Este es el proyecto numero tres',
     image: 'https://dhakasetup.com/images/300x300.png'
   },
   {
     id: 4,
     name: 'Proyecto 4',
-    url: 'https://www.google.com',
+    url: '#',
     description: 'Este es el proyecto numero cuatro',
     image: 'https://dhakasetup.com/images/300x300.png'
   },
   {
     id: 5,
     name: 'Proyecto 5',
-    url: 'https://www.google.com',
+    url: '#',
     description: 'Este es el proyecto numero cinco',
     image: 'https://dhakasetup.com/images/300x300.png'
   }
