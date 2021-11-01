f4c879b1c7d39deaaf9cad287554b11d3e2ab9f8
Lucas Gazzola <85223876+lucasjg017@users.noreply.github.com>
2021-11-01 10:54:30 -0300

    add social media to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,23 @@ export default function Footer () {
   return (
     <footer className='footer'>
       <div className='footer__container'>
-        <strong>Copyright©{year} LucasGazzola</strong>
+        <strong>Copyright © {year} - Lucas Gazzola</strong>
+        <div>
+          <ul className='footer__social-media'>
+            <li>
+              <a href='https://github.com/lucasgazzola' target='_blank' rel='noreferrer'>GitHub</a>
+            </li>
+            <li>
+              <a href='https://www.linkedin.com/in/lucasgazzola/' target='_blank' rel='noreferrer'>LinkedIn</a>
+            </li>
+            <li>
+              <a href='https://twitter.com/LucasGazzola17/' target='_blank' rel='noreferrer'>Twitter</a>
+            </li>
+            <li>
+              <a href='https://www.instagram.com/lucasgazzola/' target='_blank' rel='noreferrer'>Instagram</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </footer>
   )
