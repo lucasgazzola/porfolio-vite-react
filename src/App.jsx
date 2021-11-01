96cb8d2ab9b5b3c9fe8b44495cfb6af8c85a6d4c
Lucas Gazzola <85223876+lucasjg017@users.noreply.github.com>
2021-11-01 10:53:11 -0300

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Nav from './components/NavBar'
+import NavBar from './components/NavBar'
 import { Route } from 'wouter'
 import Home from './pages/Home'
 import About from './pages/About'
@@ -10,7 +10,7 @@ import Footer from './components/Footer'
 function App () {
   return (
     <div className='App-header'>
-      <Nav />
+      <NavBar />
       <Route path='/' component={Home} />
       <Route path='/about' component={About} />
       <Route path='/contact' component={Contact} />
